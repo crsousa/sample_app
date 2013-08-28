3aed2af1ab1136dc84aefd06a3a83b8e96e0db46
Carlos Sousa <carlos.sousa@wiremaze.com>
2013-08-28 20:47:43 +0100

    Finish sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
  def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -18,7 +19,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
   private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     name     "Michael Hartl"
     email    "michael@example.com"
-    password "foobar"
-    password_confirmation "foobar"
+    password "foobar1"
+    password_confirmation "foobar1"
   end
 end
