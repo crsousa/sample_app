ee78999fbc3b549da5c6562b8d4ed1dd29e2eb61
Carlos Sousa <carlos.sousa@wiremaze.com>
2013-08-28 15:21:11 +0100

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,20 +28,19 @@ describe User do
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: " ", password_confirmation: " ")
   end
-  it { should_not be_valid }
-end
+    it { should_not be_valid }
+  end
 
 
-describe "when password doesn't match confirmation" do
-  before { @user.password_confirmation = "mismatch" }
-  it { should_not be_valid }
-end
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
 
 
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
-
   end
 
   describe "when name is too long" do
@@ -89,6 +88,7 @@ describe "when email format is invalid" do
     before { @user.save }
     let(:found_user) { User.find_by(email: @user.email) }
 
+
     describe "with valid password" do
       it { should eq found_user.authenticate(@user.password) }
     end
@@ -99,5 +99,15 @@ describe "when email format is invalid" do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+
+    describe "email with mixed case" do
+      let(:mixed_case_email) {"foO@eXample.com"}
+
+      it "should be saved as all lower case" do
+      @user.email=mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
   end
 end
